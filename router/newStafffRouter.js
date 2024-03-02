61dd9d4b98ff9a901f401f7360a82cf2fe26af2e
TowehE <toweh02@gmail.com>
2024-03-02 21:30:09 +0100

    human resource

--- router/newStafffRouter.js
+++ router/newStafffRouter.js
@@ -23,7 +23,7 @@ const { addStaff,
 const { authenticate, authorizeRole, checkPremiumAccess } = require('../middleware/authentication');
 
 // //endpoint to send a staff a mail to join
-router.post('/addStaff/:companyId',checkPremiumAccess, addStaff)
+router.post('/addStaff/:companyId', addStaff)
 
 
 //endpoint to log in staff
@@ -49,22 +49,22 @@ router.post("/logOut/:userId", logOut)
 router.get("/search", searchStaff)
 
 //endpoint to get all staff members
-router.get('/allStaffs/:companyId',authorizeRole('admin','hod', 'cto', 'manager'),checkPremiumAccess, getAllStaffs);
+router.get('/allStaffs/:companyId',getAllStaffs);
 
 //endpoint to get a particular staff members
 router.get('/getOne/:id', aStaff);
 
 //endpoint to get all staff members
-router.get('/removeStaff/:id', authorizeRole('admin'), checkPremiumAccess,removeStaff);
+router.get('/removeStaff/:id', removeStaff);
 
 //endpoint to upload a picture
-router.put("/upload/:userId",checkPremiumAccess, uploadImage)
+router.put("/upload/:userId", uploadImage)
 
 //endpint to update staff data
-router.put("/updateStaff/:userId",checkPremiumAccess, updateProfile)
+router.put("/updateStaff/:userId", updateProfile)
       
 //endpint to update staff data  as an admin
-router.put("/updateStaffAdmin/:userId", authorizeRole('admin'), updateProfileAdmin)
+router.put("/updateStaffAdmin/:userId", updateProfileAdmin)
 
 // endpoint to vuew staff based on departmwent
 router.get('/department/:companyId/:department', getStaffByDepartmentAndCompany);
